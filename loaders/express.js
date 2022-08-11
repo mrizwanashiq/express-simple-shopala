6ce30c3748d3ced924e63e0323fd9e380d7e3a2e
mrizwanashiq <mrizwanashiq@outlook.com>
2022-08-11 22:54:32 +0500

    Removed unnecessary code

--- loaders/express.js
+++ loaders/express.js
@@ -2,7 +2,6 @@ import express from "express"
 import cors from "cors"
 import helmet from "helmet"
 import bodyParser from "body-parser"
-import passport from "passport"
 import authenticate from "../middlewares/authenticate.js"
 
 import { protectedRouter, unProtectedRouter } from "../routes/index.js"
@@ -14,7 +13,6 @@ export default async function expressLoader({ app }) {
   app.use(express.json())
   app.use(bodyParser.json())
 
-  app.use(passport.initialize())
   app.use("/api", authenticate)
 
   app.use("/api", protectedRouter)
