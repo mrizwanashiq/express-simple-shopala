4670889034a0478e250802c1740e9e8fb614fe40
mrizwanashiq <mrizwanashiq@outlook.com>
2024-08-27 21:40:01 +0500

    added multer and functionality to upload files

--- loaders/express.js
+++ loaders/express.js
@@ -1,20 +1,20 @@
-import express from "express"
-import cors from "cors"
-import helmet from "helmet"
-import bodyParser from "body-parser"
-import authenticate from "../middlewares/authenticate.js"
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import authenticate from "../middlewares/authenticate.js";
 
-import { protectedRouter, unProtectedRouter } from "../routes/index.js"
+import { protectedRouter, unProtectedRouter } from "../routes/index.js";
 
 export default async function expressLoader({ app }) {
-  app.use(cors())
-  app.use(helmet())
+  app.use(cors());
+  app.use(helmet());
+  app.use(express.static("public"));
 
-  app.use(express.json())
-  app.use(bodyParser.json())
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  // app.use(bodyParser.urlencoded());
 
-  app.use("/api", authenticate)
-
-  app.use("/api", protectedRouter)
-  app.use("/", unProtectedRouter)
+  app.use("/", unProtectedRouter);
+  app.use("/api", authenticate, protectedRouter);
 }
